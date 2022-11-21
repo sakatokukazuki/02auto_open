b57e98d445be0c6d043452cc69cd5686bccd5d90
sakatokukazuki <19223@toba-cmt.ac.jp>
2022-11-21 12:49:36 +0900

    aaa

--- open_youtube.py
+++ open_youtube.py
@@ -41,28 +41,25 @@ def get_info(driver):
     subscribers_el = driver.find_element(By.XPATH, '//*[@id="right-column"]/yt-formatted-string[3]')
     start_el = driver.find_element(By.XPATH, '//*[@id="right-column"]/yt-formatted-string[2]')
     start_day = list(start_el.text)
-    print(subscribers_el.text, *start_day[:10])
 
+def _click(driver, xpath, interval=0):
+    button = driver.find_element(By.XPATH, xpath)
+    button.click()
+    sleep(interval)
+
+def _send_keys(driver, xpath, text, interval=0):
+    output = driver.find_element(By.XPATH, xpath)
+    output.send_keys(text)
+    sleep(interval)
 
 def login(driver, mail_address, password):
-    login_el = driver.find_element(By.XPATH,'//*[@id="buttons"]/ytd-button-renderer/yt-button-shape/a/yt-touch-feedback-shape/div/div[2]')
-    login_el.click()
-    sleep(2)
-    output = driver.find_element(By.XPATH,'//*[@id="identifierId"]')
-    output.send_keys(mail_address)
-    sleep(1)
-    next = driver.find_element(By.XPATH,'//*[@id="identifierNext"]/div/button/span')
-    next.click()
-    sleep(1)
-    enter_pass = driver.find_element(By.XPATH,'//*[@id="password"]/div[1]/div/div[1]/input')
-    enter_pass.send_keys(password)
-    sleep(1)
-    next2 = driver.find_element(By.XPATH,'//*[@id="passwordNext"]/div/button/span')
-    next2.click()
-    sleep(1)
-    choise_account = driver.find_element(By.XPATH,'/html/body/ytd-app/ytd-popup-container/tp-yt-paper-dialog/ytd-channel-switcher-renderer/div[2]/div/ytd-account-item-section-renderer/div[2]/ytd-account-item-renderer[2]/tp-yt-paper-icon-item/div/yt-img-shadow')
-    choise_account.click()
-    sleep(1)
+    _click(driver, '//*[@id="buttons"]/ytd-button-renderer/yt-button-shape/a/yt-touch-feedback-shape/div/div[2]', interval=1)
+    _send_keys(driver, '//*[@id="identifierId"]', mail_address, interval=1)
+    _click(driver, '//*[@id="identifierNext"]/div/button/span', interval=1)
+    _send_keys(driver, '//*[@id="password"]/div[1]/div/div[1]/input', password, interval=1)
+    _click(driver, '//*[@id="passwordNext"]/div/button/span', interval=1)
+    _click(driver, '/html/body/ytd-app/ytd-popup-container/tp-yt-paper-dialog/ytd-channel-switcher-renderer/div[2]/div/ytd-account-item-section-renderer/div[2]/ytd-account-item-renderer[2]/tp-yt-paper-icon-item/div/yt-img-shadow', interval=1)
+
 
 if __name__ == "__main__":
     options = Options()
